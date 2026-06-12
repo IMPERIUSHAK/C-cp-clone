9afcbcb14716354cc0f48a667c83b337aa5b797e
imperiushak <komroncurabekov645@gmail.com>
2026-06-12 12:30:21 -0500

    Basic Cp implementation using pure UNIX system calls

---
+++ main.c
@@ -0,0 +1,38 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "fcntl.h"
+#include "sys/stat.h"
+
+#define MAXBUFF 10000
+
+int main(int argc, char *argv[]){
+
+
+  int f1, f2, n, m;
+  char buff[MAXBUFF];
+
+  if (argc != 4){ 
+    
+    printf("Usage: cln path1 path2\n");
+
+    return -1; 
+  }
+  
+
+  if ((f1 = open(argv[2], O_RDONLY, 0)) == -1){
+    printf("Cant open %s\n", argv[2]);
+  }
+
+  if ((f2 = creat(argv[3], 666)) == -1){
+    printf("Cant create %s\n", argv[3]);
+  }
+
+  while ((n = read(f1, buff, MAXBUFF)) > 0){
+    if ((m = write(f2, buff, n)) != n){
+      printf("Error while copy\n");
+    }
+  }
+
+  return 0;
+
+}
